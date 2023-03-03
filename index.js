3705ba712e23957d7dcd1314a36c8f4ae8dc8067
fransi-kurti <fransi.kurti@gmail.com>
2023-03-03 12:04:38 +0100

    javascript update

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const inputEl = document.getElementById("input-el");
 
+const infoTxt = document.getElementById("info-txt");
+
+const wordTitle = document.getElementById("word-title");
+
+const wordMeaning = document.getElementById("meaning");
+
+const wordDefinitionContainer = document.getElementById("word-definition-container");
+
 async function apiFetch(word){
 
     try {
+        wordDefinitionContainer.style.display = "none";
+        infoTxt.style.display = "block";
+        infoTxt.innerText = `Searching the meaning of "${word}"`;
         const dictionaryUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         const result = await fetch(dictionaryUrl).then((res) => res.json());
-        console.log(result);
+        infoTxt.style.display = "none";
+        wordDefinitionContainer.style.display = "block";
     } catch (error) {
         
     }
